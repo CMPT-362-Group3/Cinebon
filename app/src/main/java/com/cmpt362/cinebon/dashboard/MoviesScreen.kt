a5a1b9b9d377bddc7ad772ebafd164eabac186d2
Tanish Manku <tanish2k09.dev@gmail.com>
2023-11-20 05:18:12 -0800

    Update dashboard image data and bookmark layout

--- app/src/main/java/com/cmpt362/cinebon/dashboard/MoviesScreen.kt
+++ app/src/main/java/com/cmpt362/cinebon/dashboard/MoviesScreen.kt
@@ -1,8 +1,10 @@
 package com.cmpt362.cinebon.dashboard
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +19,9 @@ import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabPosition
 import androidx.compose.material3.TabRow
@@ -29,10 +33,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import com.cmpt362.cinebon.R
 import com.cmpt362.cinebon.data.DummyMovieData.newMovies
@@ -109,9 +120,9 @@ fun MoviesScreen() {
 fun MoviesGrid(source: List<MovieEntity>) {
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Fixed(2),
-        verticalItemSpacing = 8.dp,
-        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
-        modifier = Modifier.padding(8.dp),
+        verticalItemSpacing = 16.dp,
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
+        modifier = Modifier.padding(16.dp),
         content = {
             items(source.size) {
                 MovieCard(movie = source[it])
@@ -122,19 +133,52 @@ fun MoviesGrid(source: List<MovieEntity>) {
 @Composable
 fun MovieCard(movie: MovieEntity) {
 
+    val isBookmarked = rememberSaveable { mutableStateOf(false) }
+    val rememberVectorDrawablePainter = rememberVectorPainter(
+        if (isBookmarked.value)
+            ImageVector.vectorResource(id = R.drawable.bookmarked)
+        else
+            ImageVector.vectorResource(id = R.drawable.bookmark_border)
+    )
     val showShimmer = remember { mutableStateOf(true) }
 
     Card(modifier = Modifier.fillMaxSize()) {
-        AsyncImage(
-            model = R.drawable.cinebon,
-            contentDescription = movie.title,
-            contentScale = ContentScale.FillWidth,
-            onSuccess = { showShimmer.value = false },
-            modifier = Modifier
-                .background(shimmerBrush(targetValue = 1300f, showShimmer = showShimmer.value))
-                .heightIn(min = 100.dp)
-                .fillMaxSize()
-        )
+        Box(modifier = Modifier.fillMaxSize()) {
+            AsyncImage(
+                model = movie.image,
+                contentDescription = movie.title,
+                contentScale = ContentScale.FillWidth,
+                onSuccess = { showShimmer.value = false },
+                modifier = Modifier
+                    .background(shimmerBrush(targetValue = 1300f, showShimmer = showShimmer.value))
+                    .heightIn(min = 100.dp)
+                    .fillMaxSize()
+            )
+
+            Surface(
+                modifier = Modifier
+                    .zIndex(2f)
+                    .align(Alignment.TopEnd)
+                    .padding(16.dp),
+                shape = MaterialTheme.shapes.small
+            ) {
+                IconButton(
+                    onClick = {
+                        isBookmarked.value = !isBookmarked.value
+                    },
+
+                    ) {
+                    Image(
+                        painter = rememberVectorDrawablePainter,
+                        contentDescription = "Add to watchlist",
+                        colorFilter = ColorFilter.tint(if (!isBookmarked.value) MaterialTheme.colorScheme.onSurface else Color(0xFFE6BF41)),
+                        modifier = Modifier
+                            .width(32.dp)
+                            .height(32.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
