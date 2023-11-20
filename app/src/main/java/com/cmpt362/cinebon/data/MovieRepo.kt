4d895e1af3eec42b8559eac72ee12541c279d36c
dga48 <darrickgunawan@gmail.com>
2023-11-20 03:42:38 -0800

    added all images

--- app/src/main/java/com/cmpt362/cinebon/data/MovieRepo.kt
+++ app/src/main/java/com/cmpt362/cinebon/data/MovieRepo.kt
@@ -1,5 +1,7 @@
 package com.cmpt362.cinebon.data
 
+import com.cmpt362.cinebon.R
+
 object DummyMovieData {
 
     val newMovies: List<MovieEntity>
@@ -11,7 +13,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "Experience the story of THE HUNGER GAMES -- 64 years before Katniss Everdeen volunteered as tribute, and decades before Coriolanus Snow became the tyrannical President of Panem. THE HUNGER GAMES: THE BALLAD OF SONGBIRDS & SNAKES follows a young Coriolanus (Tom Blyth) who is the last hope for his failing lineage, the once-proud Snow family that has fallen from grace in a post-war Capitol. With his livelihood threatened, Snow is reluctantly assigned to mentor Lucy Gray Baird (Rachel Zegler), a tribute from the impoverished District 12. But after Lucy Gray's charm captivates the audience of Panem, Snow sees an opportunity to shift their fates. With everything he has worked for hanging in the balance, Snow unites with Lucy Gray to turn the odds in their favor. Battling his instincts for both good and evil, Snow sets out on a race against time to survive and reveal if he will ultimately become a songbird or a snake.",
-                releaseDate = "Nov 17, 2023"
+                releaseDate = "Nov 17, 2023",
+                image = R.drawable.hunger
             ),
             MovieEntity(
                 title = "TROLLS BAND TOGETHER",
@@ -20,7 +23,8 @@ object DummyMovieData {
                 ageRating = "PG",
                 review = 5,
                 description = "After two films of true friendship and relentless flirting, Poppy (Anna Kendrick) and Branch (Justin Timberlake) are now officially, finally, a couple (#broppy)! As they grow closer, Poppy discovers that Branch has a secret past. He was once part of her favorite boyband phenomenon, BroZone, with his four brothers: Floyd (Golden Globe nominated electropop sensation Troye Sivan), John Dory (Eric André; Sing 2), Spruce (Grammy winner Daveed Diggs; Hamilton) and Clay (Grammy winner Kid Cudi; Don't Look Up). BroZone disbanded when Branch was still a baby, as did the family, and Branch hasn't seen his brothers since. But when Branch's bro Floyd is kidnapped for his musical talents by a pair of nefarious pop-star villains--Velvet (Emmy winner Amy Schumer; Trainwreck) and Veneer (Grammy winner and Tony nominee Andrew Rannells; The Book of Mormon)--Branch and Poppy embark on a harrowing and emotional journey to reunite the other brothers and rescue Floyd from a fate even worse than pop-culture obscurity.",
-                releaseDate = "Nov 17, 2023"
+                releaseDate = "Nov 17, 2023",
+                image = R.drawable.trolls
             ),
             MovieEntity(
                 title = "THE MARVELS",
@@ -29,7 +33,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 4,
                 description = "Carol Danvers aka Captain Marvel has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe. When her duties send her to an anomalous wormhole linked to a Kree revolutionary, her powers become entangled with that of Jersey City super-fan, Kamala Khan aka Ms. Marvel, and Carol's estranged niece, now S.A.B.E.R. astronaut Captain Monica Rambeau. Together, this unlikely trio must team-up and learn to work in concert to save the universe as The Marvels.",
-                releaseDate = "Nov 10, 2023"
+                releaseDate = "Nov 10, 2023",
+                image = R.drawable.marvels
             ),
             MovieEntity(
                 title = "FIVE NIGHTS AT FREDDY'S",
@@ -38,7 +43,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 4,
                 description = "The film follows a troubled security guard as he begins working at Freddy Fazbear's Pizza. While spending his first night on the job, he realizes the night shift at Freddy's won't be so easy to make it through.",
-                releaseDate = "Oct 27, 2023"
+                releaseDate = "Oct 27, 2023",
+                image = R.drawable.fnaf
             ),
             MovieEntity(
                 title = "THE TUNNEL TO SUMMER, THE EXIT OF GOODBYES",
@@ -47,7 +53,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "The Urashima Tunnel can grant any wish... for a price. High school boy Kaoru, plagued by a troubled past, teams up with Anzu, a girl who struggles to place obligations before her dreams, to investigate the Tunnel. But the cost of their hearts' desires may be too high to pay. This is an unforgettable summer story of nostalgia, young love and bending time itself.",
-                releaseDate = "Nov 3, 2023"
+                releaseDate = "Nov 3, 2023",
+                image = R.drawable.tunnel
             ),
             MovieEntity(
                 title = "TAYLOR SWIFT | THE ERAS TOUR",
@@ -56,7 +63,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "The cultural phenomenon continues on the big screen! Immerse yourself in this once-in-a-lifetime concert film experience with a breathtaking, cinematic view of the history-making tour.",
-                releaseDate = "Oct 13, 2023"
+                releaseDate = "Oct 13, 2023",
+                image = R.drawable.taylor
             )
         )
 
@@ -69,7 +77,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 5,
                 description = "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-                releaseDate = "Nov 1, 2019"
+                releaseDate = "Nov 1, 2019",
+                image = R.drawable.parasite
             ),
             MovieEntity(
                 title = "CASABLANCA",
@@ -78,7 +87,8 @@ object DummyMovieData {
                 ageRating = "PG",
                 review = 5,
                 description = "Rick Blaine (Humphrey Bogart), who owns a nightclub in Casablanca, discovers his old flame Ilsa (Ingrid Bergman) is in town with her husband, Victor Laszlo (Paul Henreid). Laszlo is a famed rebel, and with Germans on his tail, Ilsa knows Rick can help them get out of the country.",
-                releaseDate = "Jan 23, 1943"
+                releaseDate = "Jan 23, 1943",
+                image = R.drawable.casablanca
             ),
             MovieEntity(
                 title = "ALL ABOUT EVE",
@@ -87,7 +97,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 5,
                 description = "Backstage story revolving around aspiring actress Eve Harrington. Tattered and forlorn, Eve shows up in the dressing room of Broadway mega-star Margo Channing, telling a melancholy life story to Margo and her friends. Margo takes Eve under her wing, and it appears that Eve is a conniver that uses Margo.",
-                releaseDate = "Oct 13, 1950"
+                releaseDate = "Oct 13, 1950",
+                image = R.drawable.eve
             ),
             MovieEntity(
                 title = "ON THE WATERFRONT",
@@ -96,7 +107,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "Dockworker Terry Malloy (Marlon Brando) had been an up-and-coming boxer until powerful local mob boss Johnny Friendly (Lee J. Cobb) persuaded him to throw a fight. When a longshoreman is murdered before he can testify about Friendly's control of the Hoboken waterfront, Terry teams up with the dead man's sister Edie (Eva Marie Saint) and the streetwise priest Father Barry (Karl Malden) to testify himself, against the advice of Friendly's lawyer, Terry's older brother Charley (Rod Steiger).",
-                releaseDate = "Jul 28, 1954"
+                releaseDate = "Jul 28, 1954",
+                image = R.drawable.waterfront
             ),
             MovieEntity(
                 title = "MOONLIGHT",
@@ -105,7 +117,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 4,
                 description = "A look at three defining chapters in the life of Chiron, a young black man growing up in Miami. His epic journey to manhood is guided by the kindness, support and love of the community that helps raise him.",
-                releaseDate = "Nov 18, 2016"
+                releaseDate = "Nov 18, 2016",
+                image = R.drawable.moonlight
             ),
             MovieEntity(
                 title = "SCHINDLER'S LIST",
@@ -114,7 +127,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 5,
                 description = "Businessman Oskar Schindler (Liam Neeson) arrives in Krakow in 1939, ready to make his fortune from World War II, which has just started. After joining the Nazi party primarily for political expediency, he staffs his factory with Jewish workers for similarly pragmatic reasons. When the SS begins exterminating Jews in the Krakow ghetto, Schindler arranges to have his workers protected to keep his factory in operation, but soon realizes that in so doing, he is also saving innocent lives.",
-                releaseDate = "Dec 15, 1993"
+                releaseDate = "Dec 15, 1993",
+                image = R.drawable.list
             )
         )
 
@@ -127,7 +141,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 3,
                 description = "Napoleon is a spectacle-filled action epic that details the checkered rise and fall of the iconic French Emperor Napoleon Bonaparte, played by Oscar winner Joaquin Phoenix. Against a stunning backdrop of large-scale filmmaking orchestrated by legendary director Ridley Scott, the film captures Bonaparte's relentless journey to power through the prism of his addictive, volatile relationship with his one true love, Josephine, showcasing his visionary military and political tactics against some of the most dynamic practical battle sequences ever filmed.",
-                releaseDate = "Nov 22, 2023"
+                releaseDate = "Nov 22, 2023",
+                image = R.drawable.napoleon
             ),
             MovieEntity(
                 title = "WISH",
@@ -136,7 +151,8 @@ object DummyMovieData {
                 ageRating = "PG",
                 review = 3,
                 description = "Walt Disney Animation Studios' Wish is an all-new musical-comedy welcoming audiences to the magical kingdom of Rosas, where Asha, a sharp-witted idealist, makes a wish so powerful that it is answered by a cosmic force--a little ball of boundless energy called Star. Together, Asha and Star confront a most formidable foe--the ruler of Rosas, King Magnifico--to save her community and prove that when the will of one courageous human connects with the magic of the stars, wondrous things can happen.",
-                releaseDate = "Nov 22, 2023"
+                releaseDate = "Nov 22, 2023",
+                image = R.drawable.wish
             ),
             MovieEntity(
                 title = "THE BOY AND THE HERON",
@@ -145,7 +161,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 0,
                 description = "A young boy named Mahito yearning for his mother ventures into a world shared by the living and the dead. There, death comes to an end, and life finds a new beginning. A semi-autobiographical fantasy about life, death, and creation, in tribute to friendship, from the mind of Hayao Miyazaki.",
-                releaseDate = "Dec 8, 2023"
+                releaseDate = "Dec 8, 2023",
+                image = R.drawable.heron
             ),
             MovieEntity(
                 title = "MAESTRO",
@@ -154,7 +171,8 @@ object DummyMovieData {
                 ageRating = "R",
                 review = 4,
                 description = "Maestro is a towering and fearless love story chronicling the lifelong relationship between Leonard Bernstein and Felicia Montealegre Cohn Bernstein. A love letter to life and art, Maestro at its core is an emotionally epic portrayal of family and love.",
-                releaseDate = "Nov 22, 2023"
+                releaseDate = "Nov 22, 2023",
+                image = R.drawable.maestro
             ),
             MovieEntity(
                 title = "MENUS-PLAISIRS: LES TROISGROS",
@@ -163,7 +181,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "Founded in 1930 in central France, the Troisgros family restaurant has been holding 3 Michelin stars for 55 years over four generations. Michel Troisgros, the third generation to head the restaurant, has turned over the responsibility for the cuisine to his son César, the 4th generation of Troisgros chefs. From the market to pick fresh vegetables, to a cheese processing plant, a vineyard, an organic cattle ranch to the backyard garden supplying the restaurant, Wiseman embarks us on a mouthwatering and sense-pleasing journey into the family’s three restaurant kitchens. An immersive experience, showing the great artistry, ingenuity, imagination, and hard work of the restaurant staff in creating, preparing, and presenting meals of the highest quality.",
-                releaseDate = "Nov 22, 2023"
+                releaseDate = "Nov 22, 2023",
+                image = R.drawable.menus
             ),
             MovieEntity(
                 title = "MONSTER",
@@ -172,7 +191,8 @@ object DummyMovieData {
                 ageRating = "PG-13",
                 review = 5,
                 description = "When her young son Minato starts to behave strangely, his mother feels that there is something wrong. Discovering that a teacher is responsible, she storms into the school demanding to know what's going on. But as the story unfolds through the eyes of mother, teacher and child, the truth gradually emerges.",
-                releaseDate = "Nov 22, 2023"
+                releaseDate = "Nov 22, 2023",
+                image = R.drawable.monster
             )
         )
 }
