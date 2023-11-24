1f6540eed9ba19272069a3a878f6038aae31d006
Shabbir Yusufali <shabyusufali@gmail.com>
2023-11-23 22:39:03 -0800

    Firebase auth (#12)
    * Setup and Save work
    
    * update code
    
    * Complete Signup and Signin
    
    * Fix Naming Convention
    
    * Forgot Password and Error Handling

---
+++ app/src/main/java/com/cmpt362/cinebon/viewmodels/UserAuthViewModel.kt
@@ -0,0 +1,66 @@
+package com.cmpt362.cinebon.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+
+interface AccountService {
+    fun signUp(email: String, password: String, fName: String, lName: String, onResult: (Throwable?) -> Unit)
+    fun signIn(email: String, password: String, onResult: (Throwable?) -> Unit)
+    fun sendResetPasswordEmail(email: String, onResult: (Throwable?) -> Unit)
+}
+
+class UserAuthViewModel: ViewModel(), AccountService {
+    private val auth = FirebaseAuth.getInstance()
+
+    fun isSignedIn(): Boolean {
+        val currentUser = auth.currentUser
+        return if (currentUser != null) {
+            Log.d("UserAuthViewModel", "User is signed in")
+            true
+        } else {
+            Log.d("UserAuthViewModel", "User is not signed in")
+            false
+        }
+    }
+
+    override fun signIn(email: String, password: String, onResult: (Throwable?) -> Unit) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("AccountService", "User signed in successfully")
+                    onResult(null)
+                } else {
+                    Log.d("AccountService", "User sign in failed")
+                    onResult(task.exception)
+                }
+            }
+    }
+
+    override fun signUp(email: String, password: String, fName: String, lName: String, onResult: (Throwable?) -> Unit) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("AccountService", "User created successfully")
+                    onResult(null)
+                } else {
+                    Log.d("AccountService", "User creation failed")
+                    onResult(task.exception)
+                }
+            }
+    }
+
+    override fun sendResetPasswordEmail(email: String, onResult: (Throwable?) -> Unit) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("AccountService", "Reset password email sent successfully")
+                    onResult(null)
+                } else {
+                    Log.d("AccountService", "Reset password email failed to send")
+                    onResult(task.exception)
+                }
+            }
+    }
+}
