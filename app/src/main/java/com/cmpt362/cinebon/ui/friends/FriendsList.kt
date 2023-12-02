5d9dfc79526ddce8317ca7511b340c667b243772
shabbiryusufali <shabyusufali@gmail.com>
2023-12-02 00:39:27 -0800

    Add Screen

---
+++ app/src/main/java/com/cmpt362/cinebon/ui/friends/FriendsList.kt
@@ -0,0 +1,71 @@
+package com.cmpt362.cinebon.ui.friends
+
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cmpt362.cinebon.R
+import com.cmpt362.cinebon.ui.destinations.DashboardNavDestination
+import com.cmpt362.cinebon.ui.destinations.LoginScreenDestination
+import com.cmpt362.cinebon.ui.destinations.SignupScreenDestination
+import com.cmpt362.cinebon.ui.signup.SignupScreen
+import com.cmpt362.cinebon.ui.theme.CinebonTheme
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import com.ramcosta.composedestinations.navigation.popUpTo
+
+@Destination
+@Composable
+fun FriendsListScreen() {
+
+    val scrollState = rememberScrollState()
+
+    Surface(
+        modifier = Modifier
+            .scrollable(scrollState, Orientation.Vertical)
+            .fillMaxSize(), color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun FriendsListPreview() {
+    CinebonTheme {
+        FriendsListScreen()
+    }
+}
