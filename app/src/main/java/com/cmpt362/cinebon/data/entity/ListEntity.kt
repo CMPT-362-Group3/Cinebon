c49d8e46c361d4a43bc8ed94c5161774a80a49c6
dga48 <darrickgunawan@gmail.com>
2023-12-03 18:50:50 -0800

    updated list entity

--- app/src/main/java/com/cmpt362/cinebon/data/entity/ListEntity.kt
+++ app/src/main/java/com/cmpt362/cinebon/data/entity/ListEntity.kt
@@ -1,5 +1,6 @@
 package com.cmpt362.cinebon.data.entity
 
+import com.cmpt362.cinebon.data.objects.User
 import com.google.firebase.firestore.DocumentReference
 
 class ListEntity {
@@ -7,3 +8,9 @@ class ListEntity {
     lateinit var listName: String
     var movies = mutableListOf<Int>()
 }
+
+data class ResolvedListEntity (
+    val owner: User,
+    val listName: String,
+    val movies: MutableList<Int> = mutableListOf()
+)
