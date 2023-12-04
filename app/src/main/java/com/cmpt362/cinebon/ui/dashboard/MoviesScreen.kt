c745265f46448ef2beee8f56304a13caccb7ea18
Tanish Manku <tanish2k09.dev@gmail.com>
2023-12-04 05:52:58 -0800

    Update Movie composables to be externally re-useable

--- app/src/main/java/com/cmpt362/cinebon/ui/dashboard/MoviesScreen.kt
+++ app/src/main/java/com/cmpt362/cinebon/ui/dashboard/MoviesScreen.kt
@@ -1,6 +1,7 @@
 package com.cmpt362.cinebon.ui.dashboard
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -119,17 +120,25 @@ fun MoviesGrid(source: List<Movie>, onClick: (Movie) -> Unit) {
         modifier = Modifier.padding(16.dp),
         content = {
             items(source.size) {
-                MovieCard(movie = source[it], onClick = { onClick(source[it]) })
+                MovieCard(movie = source[it], onClick)
             }
         })
 }
 
 @Composable
-fun MovieCard(movie: Movie, onClick: () -> Unit) {
-    Card(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            MovieImage(movie, onClick)
-            MovieBookmarkIcon(movie, modifier = Modifier.align(Alignment.TopEnd))
+fun MovieCard(
+    movie: Movie,
+    onClick: (Movie) -> Unit,
+    modifier: Modifier = Modifier,
+    showQuickAdd: Boolean = false,
+    onQuickAdd: (Movie) -> Unit = {}) {
+    Card(modifier = modifier.fillMaxSize()) {
+        Box(modifier = modifier.fillMaxSize()) {
+            MovieImage(movie, onClick = { onClick(movie) })
+
+            if (showQuickAdd) {
+                MovieBookmarkIcon(movie, modifier = modifier.align(Alignment.TopEnd).clickable { onQuickAdd(movie) })
+            }
         }
     }
 }
