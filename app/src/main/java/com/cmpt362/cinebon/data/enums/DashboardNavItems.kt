60390fe6277be656a9cb31056b16e099238e3910
Tanish Manku <tanish2k09.dev@gmail.com>
2023-12-04 21:58:04 -0800

    Fix the navigation page for ProfileScreen

--- app/src/main/java/com/cmpt362/cinebon/data/enums/DashboardNavItems.kt
+++ app/src/main/java/com/cmpt362/cinebon/data/enums/DashboardNavItems.kt
@@ -1,13 +1,13 @@
 package com.cmpt362.cinebon.data.enums
 
 import com.cmpt362.cinebon.R
-import com.cmpt362.cinebon.ui.destinations.ChatListScreenDestination
 import com.cmpt362.cinebon.ui.destinations.DirectionDestination
 import com.cmpt362.cinebon.ui.destinations.ListsScreenDestination
 import com.cmpt362.cinebon.ui.destinations.MoviesScreenDestination
+import com.cmpt362.cinebon.ui.destinations.ProfileScreenDestination
 
 enum class DashboardNavItems(val icon: Int, val destination: DirectionDestination) {
     Movies(R.drawable.movie, MoviesScreenDestination),
     Lists(R.drawable.lists, ListsScreenDestination),
-    Profile(R.drawable.profile, ChatListScreenDestination)
+    Profile(R.drawable.profile, ProfileScreenDestination)
 }
