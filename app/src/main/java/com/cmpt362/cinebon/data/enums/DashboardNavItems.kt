dc9f8b450e5acc75bc9d73e344f68c79bc27f711
Tanish Manku <tanish2k09.dev@gmail.com>
2023-12-05 23:37:03 -0800

    Add comments to enums

--- app/src/main/java/com/cmpt362/cinebon/data/enums/DashboardNavItems.kt
+++ app/src/main/java/com/cmpt362/cinebon/data/enums/DashboardNavItems.kt
@@ -4,9 +4,9 @@ import com.cmpt362.cinebon.R
 import com.cmpt362.cinebon.ui.destinations.DirectionDestination
 import com.cmpt362.cinebon.ui.destinations.ListsScreenDestination
 import com.cmpt362.cinebon.ui.destinations.MoviesScreenDestination
-import com.cmpt362.cinebon.ui.destinations.ProfileScreenDestination
 import com.cmpt362.cinebon.ui.destinations.SocialScreenDestination
 
+// Handy enum class for the dashboard navigation items
 enum class DashboardNavItems(val icon: Int, val destination: DirectionDestination) {
     Movies(R.drawable.movie, MoviesScreenDestination),
     Lists(R.drawable.lists, ListsScreenDestination),
--- app/src/main/java/com/cmpt362/cinebon/data/enums/FriendRequestStatus.kt
+++ app/src/main/java/com/cmpt362/cinebon/data/enums/FriendRequestStatus.kt
@@ -1,5 +1,7 @@
 package com.cmpt362.cinebon.data.enums
 
+// Enum class to represent the state of "friendship" between two users
+// This is used to determine what to display on the friend profile screen page
 enum class FriendRequestStatus {
     SENT, // Request sent, not accepted
     RECEIVED, // Request received, not accepted
